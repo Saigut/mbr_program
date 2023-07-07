31143178af4fd0659b8392943c5a82d2f2092a23
Saigut <gosaigut@gmail.com>
2023-07-07 15:17:54 +0800

    fix tty output on real pc, use si + lodsb; add instructions at the beginning of the program; update comments

--- mbr_program.c
+++ mbr_program.c
@@ -1,43 +1,59 @@
+void init_func() {
+    asm volatile (
+            "cli\n"
+            "xorw %%ax, %%ax\n"
+            "movw %%ax, %%ds\n"
+            "movw %%ax, %%ss\n"
+            "movw %%ax, %%es\n"
+            "call main_func\n"
+            :
+            :
+            : "ax"
+            );
+}
+
+
 #include <stdint.h>
 
-
-#define REGPARM __attribute__ ((regparm(3)))
 #define SECTION(s) __attribute__((section(s)))
 
-REGPARM void print_str(const char* str);
+void print_str(const char* str);
 void print_block();
 void sleep_one_second();
 
-SECTION(".text") void main_func()
+
+SECTION(".text")
+void main_func()
 {
-    const char* message = "Hello, World!\n";
+    const char* message = "Hello, World!";
+
     print_str(message);
     sleep_one_second();
     sleep_one_second();
     print_block();
+
     while(1) {
         asm volatile ("hlt");
     }
 }
 
-REGPARM void print_str(const char* str)
+void print_str(const char* str)
 {
-    int i = 0;
-    while (*str && i < 32) {
-        asm volatile (
-                "mov %0, %%ebx\n"
-                "movb 0x0000(%%bx), %%al\n"
-                "movb $0x0E, %%ah\n"
-                "movb $0x00, %%bh\n"
-                "movb $0x07, %%bl\n"
-                "int $0x10\n"
-                :
-                : "r" (str)
-                : "%ah", "%bh", "%bl"
-                );
-        str++;
-        i++;
-    }
+    asm volatile (
+            "movw %%bx, %%si\n"
+            "1:\n"
+            "lodsb\n"
+            "cmpb $0, %%al\n"
+            "je 2f\n"
+            "movb $0xe, %%ah\n"
+            "movw $0x0001, %%bx\n"
+            "int $0x10\n"
+            "jmp 1b\n"
+            "2:\n"
+            :
+            : "b" (str)
+            : "%ah", "%al"
+            );
 }
 
 void print_block()
@@ -45,14 +61,14 @@ void print_block()
     uint8_t color;
     uint16_t i = 0;
 
-    // 配置 vga
+    // setup vga
     asm volatile (
-            // 配置 vga 中断
+            // setup interrupt of vga
             "mov $0, %%ah\n"
             "mov $0x13, %%al\n"
             "int $0x10\n"
 
-            // 设置 vga 地址
+            // set address of vga
             "mov $0xa000, %%bx\n"
             "mov %%bx, %%ds\n"
             :
@@ -60,11 +76,11 @@ void print_block()
             : "%ax", "%bx"
             );
 
-    // 着色
+    // draw
     while (i < 64000) {
         color = i >> 8;
         asm volatile (
-                // 写入颜色
+                // draw color
                 "mov %0, %%al\n"
                 "mov %1, %%bx\n"
                 "mov %%al, %%ds:(%%bx)\n"
@@ -76,13 +92,13 @@ void print_block()
     }
 }
 
-// 粗略的1秒，不精确
+// Roughly 1 second, not precise
 void sleep_one_second() {
     asm volatile (
             "movb $0x86, %%ah;"   /* BIOS Wait */
             "xorw %%bx, %%bx;"    /* Clear BX */
             "movw $0x09, %%cx;"   /* High word of delay */
-            "movw $0x27c0, %%dx;" /* Low word of delay (cx:dx = 0x0F * 2^16 + 0x4240 = 1,000,000 microseconds or 1 second) */
+            "movw $0x27c0, %%dx;" /* Low word of delay (eg: cx:dx = 0x0F * 2^16 + 0x4240 = 1,000,000 microseconds or 1 second) */
             "int $0x15;"          /* BIOS interrupt */
             :
             :
